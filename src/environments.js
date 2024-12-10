2290c1b3024e64b7f84a2994c574917363c91344
SantiagoMurilloV <128425308+SantiagoMurilloV@users.noreply.github.com>
2024-12-10 18:50:14 -0500

    fix and delete folders

--- src/environments.js
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: false,
-  apiURL: 'http://localhost:8084', // Ruta de tu servidor backend de desarrollo]
-  // apiURL:'https://sea-turtle-app-wouf5.ondigitalocean.app'
-
-};
